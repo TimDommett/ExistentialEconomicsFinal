48b774ed8f214ce7e741d5044fdafef3a9e30261
Tim Dommett <timdom11@gmail.com>
2019-02-25 02:10:28 +0200

    updating for heroku

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,5 +2,5 @@ class Article < ActiveRecord::Base
   validates :title, presence: true, length: { minimum: 3, maximum: 200 }
   validates :description, presence: true, length: { minimum: 10}
   # validates :user_id, presence: true
-  belongs_to :user
+  # belongs_to :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ActiveRecord::Base
 
   validates :username, presence: true, uniqueness: { case_sensitive: false }, length: { minimum: 3, maximum: 25 }, format: { with: VALID_EMAIL_REGEX }
   validates :email, presence: true, uniqueness: true
-  has_many :articles
+  # has_many :articles
   has_secure_password
   before_save { self.email = email.downcase }
 
