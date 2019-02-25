f3fef59618e0efcebb5dd35f2f145d26de904bd2
Tim Dommett <timdom11@gmail.com>
2019-02-25 02:07:57 +0200

    updating for heroku

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,6 @@
 class Article < ActiveRecord::Base
   validates :title, presence: true, length: { minimum: 3, maximum: 200 }
   validates :description, presence: true, length: { minimum: 10}
-  validates :user_id, presence: true
+  # validates :user_id, presence: true
   belongs_to :user
 end
